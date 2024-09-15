40971e468070fa66c4dda18cd4662edf1cfcef87
AlaaAraydah3 <alaaaraydah50@gmail.com>
2024-09-16 01:51:56 +0300

    Update memory.sv

--- RTL/memory.sv
+++ RTL/memory.sv
@@ -3,7 +3,7 @@ module mem (
     input logic [3:0] Address,
     input logic [31:0] Data_in,
     input logic Rw_en,
-	  input logic Rr_en,
+    input logic Rr_en,
     output logic [31:0] Data_out,
     output logic Valid_out
     );
@@ -12,16 +12,18 @@ module mem (
 
   always @( posedge clk  ) begin
         if (rst) begin
-            Valid_out <= 1'bx;
+            Valid_out <= 1'bx ;
             Data_out  <= 32'bx;
+		
         end else if (En && Rr_en) begin
             // Read operation
-            Valid_out <= 1;
+            Valid_out <= 1'b1;
             Data_out  <= memory[Address];
+		
         end else if (En && Rw_en) begin
             // Write operation
             memory[Address] <= Data_in;
-            Valid_out       <= 0; 
+            Valid_out       <= 1'b0   ; 
         end 
     end
 endmodule
