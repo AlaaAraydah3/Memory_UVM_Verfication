d3c6fb492b9f6f3aa2441eafe8837867ba6ff152
AlaaAraydah3 <alaaaraydah50@gmail.com>
2024-09-23 05:08:07 +0300

    Update top.sv

--- UVM/top.sv
+++ UVM/top.sv
@@ -1,12 +1,18 @@
 
-`include "intf.sv"
+`include "uvm_macros.svh"
 `include "memory.sv"
+`include "intf.sv"
+import uvm_pkg::*;
+
 module top;
   bit clk;
   always #5 clk = ~clk;
+
   import memory_pkg::*;
   intf in1 (clk);
+  // Instantiate the memory module and pass the interface
   mem mem_inst (.in1(in1));
+
   initial begin
     clk = 0;
     uvm_config_db#(virtual intf)::set(null, "uvm_test_top", "my_vif", in1);
