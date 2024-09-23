20f77fa6a4827dba3dc8069deb0a624ad3cca7c5
AlaaAraydah3 <alaaaraydah50@gmail.com>
2024-09-23 05:15:01 +0300

    Update my_sequence_item.sv

--- UVM/my_sequence_item.sv
+++ UVM/my_sequence_item.sv
@@ -1,4 +1,5 @@
 
+
 class my_sequence_item extends uvm_sequence_item;
    `uvm_object_utils(my_sequence_item)
 
@@ -13,9 +14,10 @@ class my_sequence_item extends uvm_sequence_item;
   randc logic  [3:0] Address  ;
   rand  logic  [31:0] Data_in ;
   
-  
+
     constraint Rw_Rr       {{Rr_en != Rw_en};}
-    constraint rst_en      { rst     inside {0,1};}
-    constraint address     {Address  inside {[0:15]};}
-    constraint data_in     {Data_in  dist {[0 :600]:/50, [601 : 900]:/40, [901 : $]:/50};}
+    constraint rst_en      { rst     dist {0:=50,1:=5};}
+
+    constraint address     {Address  dist {[0:15]:/100};}
+    constraint data_in     {Data_in  dist {32'h0:=0.5, [32'd1:32'd254]:/100 ,32'd255:=0.5};} 
 endclass
