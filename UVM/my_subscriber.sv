02d9701ce35f291ab9cefc1abb46bdb4ca14998d
AlaaAraydah3 <alaaaraydah50@gmail.com>
2024-09-23 05:12:47 +0300

    Update my_subscriber.sv

--- UVM/my_subscriber.sv
+++ UVM/my_subscriber.sv
@@ -1,3 +1,4 @@
+
 class my_subscriber extends uvm_subscriber#(my_sequence_item);
   `uvm_component_utils(my_subscriber)
    my_sequence_item seq_item;
@@ -7,59 +8,55 @@ class my_subscriber extends uvm_subscriber#(my_sequence_item);
   covergroup group_1;
         Address: coverpoint seq_item.Address 
          {
-          bins addr_bins = {[0:15]};  
+          bins addr_bins[] = {[0:15]};  
          }
     
         rst: coverpoint seq_item.rst 
         {
          bins rst_L    = {0};
          bins rst_H    = {1};
-         bins rst_L2H  = (0=>1); 
-         bins rst_H2L  = (1=>0);
+        // bins rst_L2H  = (0=>1); 
+        // bins rst_H2L  = (1=>0);
          }
     
          Rr_en: coverpoint seq_item.Rr_en
          {
          bins read_L    = {0};
          bins read_H    = {1};
-         bins read_L2H  = (0=>1); 
-         bins read_H2L  = (1=>0);
+
          }
     
          Rw_en: coverpoint seq_item.Rw_en
          {
           bins write_L   = {0};
           bins write_H   = {1};
-          bins write_L2H = (0=>1); 
-          bins write_H2L = (1=>0);
+
           }
 
           En: coverpoint seq_item.En 
           {
-           bins en_L   = {0};
+          
            bins en_H   = {1};
-           bins en_L2H = (0=>1); 
-           bins en_H2L = (1=>0);
+
           }
     
           Valid_out : coverpoint seq_item.Valid_out
           {
            bins V_L   = {0};
            bins V_H   = {1};
-           bins V_L2H = (0=>1);
-           bins V_H2L = (1=>0);
+
           }
     
           Data_in: coverpoint seq_item.Data_in
           {
-          bins data_in1 = {[0:$]};
+          bins data_in = {[0:255]};
           }
     
-          Data_out: coverpoint seq_item.Data_out 
-          {
-          bins data_o1 = {[0:$]};
-          }
-          // cross1: cross Address, Data_in ;
+          
+          
+          cross1: cross Address, Data_in, En, Rw_en;// check write
+          cross2: cross Address, Data_in, En, Rr_en;// check read
+          cross3: cross Data_in, rst;               // check reset
   endgroup
 
    
@@ -68,8 +65,8 @@ class my_subscriber extends uvm_subscriber#(my_sequence_item);
     group_1 = new();
   endfunction
   
-  function void write(input my_sequence_item seq_item);
-    this.seq_item = seq_item; // Assign the sequence item to class-level variable
+  function void write(input my_sequence_item t);
+    this.seq_item = t; // Assign the sequence item to class-level variable
     // Sample the coverage group with the sequence item
     group_1.sample();
   endfunction
@@ -90,7 +87,7 @@ class my_subscriber extends uvm_subscriber#(my_sequence_item);
   function void report_phase(uvm_phase phase);
     super.report_phase(phase);
     final_cover = group_1.get_coverage();
-    `uvm_info(get_type_name(), $sformatf("Final Coverage is: %0f", final_cov), UVM_LOW);
+    `uvm_info(get_type_name(), $sformatf("Final Coverage is: %0f", final_cover), UVM_LOW);
     
   endfunction
  
